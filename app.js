9ca391ca12d9a400e8368975c6cd427a4376a8ef
Neil <neil@logan-5.co.uk>
2026-06-19 23:19:26 +0100

    Bump app and service worker versions to 0.1.8

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import { EXAMPLES, buildParseTree, parseGrammar, renderDiagramSvg, testString } from "./syntaxor-core.js";
 
-const APP_VERSION = "0.1.7";
+const APP_VERSION = "0.1.8";
 const STORAGE_KEY = "syntaxor.workspace.v1";
 const ABOUT_SHOW_ON_START_KEY = "syntaxor.about.showOnStart";
 const DEFAULT_EXAMPLE_KEY = "arithmetic";
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "0.1.7";
+const CACHE_VERSION = "0.1.8";
 const CACHE_NAME = `syntaxor-shell-${CACHE_VERSION}`;
 const APP_SHELL = [
   "./",
