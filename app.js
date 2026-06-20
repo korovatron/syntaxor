c2878db8628bce6280f2e80033b98bb8d058a418
Neil <neil@logan-5.co.uk>
2026-06-20 15:28:24 +0100

    Bump version to 1.0.11

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ import {
   historyKeymap
 } from "./vendor/codemirror.js";
 
-const APP_VERSION = "1.0.10";
+const APP_VERSION = "1.0.11";
 const STORAGE_KEY = "syntaxor.workspace.v1";
 const ABOUT_SHOW_ON_START_KEY = "syntaxor.about.showOnStart";
 const DEFAULT_EXAMPLE_KEY = "arithmetic";
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "1.0.10";
+const CACHE_VERSION = "1.0.11";
 const CACHE_NAME = `syntaxor-shell-${CACHE_VERSION}`;
 const APP_SHELL = [
   "./",
