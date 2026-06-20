144f3fdca88e873eccda4669020cdedea15ef660
Neil <neil@logan-5.co.uk>
2026-06-20 15:23:16 +0100

    Add blank lines between rules and split digit rule into 2 lines

--- syntaxor-core.js
+++ syntaxor-core.js
@@ -7,12 +7,17 @@ export const EXAMPLES = {
       '<expression> ::= | <factor>',
       '                 | <factor> "*" <factor>',
       '                 | <factor> "/" <factor>',
+      '',
       '<factor> ::= | <term>',
       '             | <term> "+" <term>',
       '             | <term> "-" <term>',
+      '',
       '<term> ::= "-" <expression> | <number>',
+      '',
       '<number> ::= <digit> | <digit> <number>',
-      '<digit> ::= "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"'
+      '',
+      '<digit> ::= | "0" | "1" | "2" | "3" | "4"',
+      '             | "5" | "6" | "7" | "8" | "9"'
     ].join("\n")
   },
   identifier: {
