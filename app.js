987b34ddd9aad5c7454be8cec98d9a2d6fb830d9
Neil <neil@logan-5.co.uk>
2026-06-20 19:43:38 +0100

    v1.0.16 - add PNG icons to manifest for Android and Windows

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ import {
   historyKeymap
 } from "./vendor/codemirror.js";
 
-const APP_VERSION = "1.0.15";
+const APP_VERSION = "1.0.16";
 const STORAGE_KEY = "syntaxor.workspace.v1";
 const ABOUT_SHOW_ON_START_KEY = "syntaxor.about.showOnStart";
 const DEFAULT_EXAMPLE_KEY = "arithmetic";
